fbb4a4c84d7bd5f0e0e5376c71db849ca9e6e82d
Juan Pablo <jpmontoya19@gmail.com>
2022-02-25 16:32:19 -0500

    Object Oriented Programming Course

---
+++ POO_Platzi/Java/Car.java
@@ -0,0 +1,54 @@
+class Car {
+    private Integer id;
+    private String license;
+    private Driver driver;
+    protected Integer passenger;
+    
+    public Car(String license, Driver driver) {
+        this.license = license;
+        this.driver = driver;
+    }
+
+    void printDataCar() {
+        if (passenger != null) {
+            System.out.println("License: " + license + " Name Driver: " + driver.name + " Passengers: " + passenger);
+        }
+    }
+
+    public Integer getPassenger() {
+        return passenger;
+    }
+
+    public void setPassenger(Integer passenger) {
+        if (passenger == 4) {
+            this.passenger = passenger;
+        }
+        else {
+            System.out.println("You need to enter 4 passengers");
+        }
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    } 
+}
---
+++ POO_Platzi/PHP/uberVan.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once('car.php');
+
+class UberVan extends Car {
+    public $typeCarAccepted;
+    public $seatsMaterial;
+
+    public function __construct($license, $driver, $typeCarAccepted, $seatsMaterial) {
+        parent::__construct($license, $driver);
+        $this->typeCarAccepted = $typeCarAccepted;
+        $this->seatsMaterial = $seatsMaterial;
+    }
+
+    public function setPassenger($passenger) {
+        if ($passenger == 6) {
+            $this->passenger = $passenger;
+        } 
+        else {
+            echo "You need to assign 6 passengers";    
+        }
+        
+    }
+}
+
+?>
