b15ae103212f180144f36818585a303823fa6b20
Juan Pablo <jpmontoya19@gmail.com>
2022-03-01 11:50:53 -0500

    Generator ret

--- Python_Professional/generators.py
+++ Python_Professional/generators.py
@@ -1,25 +1,13 @@
 import time
 
-def fibo_gen():
-    n1 = 0
-    n2 = 1
-    counter = 0
-
-    while True:
-        if counter == 0:
-            counter += 1
-            yield n1
-        elif counter == 1:
-            counter += 1
-            yield n2
-        else:
-            aux = n1 + n2
-            n1, n2 = n2, aux
-            counter += 1
-            yield aux
+def fibo_gen(max=None):
+    n1, n2 = 0, 1
+    while not max or max >= n1:
+        yield n1
+        n1, n2 = n2, n1 + n2
 
 if __name__ == "__main__":
-    fibonacci = fibo_gen()
+    fibonacci = fibo_gen(20)
     for element in fibonacci:
         print(element)
         time.sleep(1)
