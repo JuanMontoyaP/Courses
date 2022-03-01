d163201580a4b7305686d5aef305b0b772e8a318
Juan Pablo <jpmontoya19@gmail.com>
2022-03-01 00:29:32 -0500

    Iterator ret

--- Python_Professional/iterators.py
+++ Python_Professional/iterators.py
@@ -2,9 +2,13 @@ import time
 
 class FiboIter():
 
+    def __init__(self, max=None):
+        self.max = max
+
     def __iter__(self):
         self.n1 = 0
         self.n2 = 1
+        self.aux = 0
         self.counter = 0
         return self
 
@@ -13,18 +17,46 @@ class FiboIter():
             self.counter += 1
             return self.n1
         elif self.counter == 1:
-            self.counter += 1
-            return self.n2
+            self.counter += 1  
+            if self.max != 0:
+                return self.n2
+            else: 
+                raise StopIteration
         else:
             self.aux = self.n1 + self.n2
             # self.n1 = self.n2
             # self.n2 = self.aux
             self.n1, self.n2 = self.n2, self.aux
             self.counter += 1
-            return self.aux
+            if not self.max or self.aux <= self.max:
+                return self.aux
+            else:
+                raise StopIteration
+    
 
 if __name__ == "__main__":
-    fibonacci = FiboIter()
-    for element in fibonacci:
+    fibonacci_1 = FiboIter(0)
+    for element in fibonacci_1:
         print(element)
-        time.sleep(0.05)
+        time.sleep(0.5)
+
+    print("---------------------------------------------")
+    
+    fibonacci_2 = FiboIter(1)
+    for element in fibonacci_2:
+        print(element)
+        time.sleep(0.5)
+
+    print("---------------------------------------------")
+
+    fibonacci_3 = FiboIter(20)
+    for element in fibonacci_3:
+        print(element)
+        time.sleep(0.5)
+
+    print("---------------------------------------------")
+
+    fibonacci_4 = FiboIter()
+    for element in fibonacci_4:
+        print(element)
+        time.sleep(0.5)
