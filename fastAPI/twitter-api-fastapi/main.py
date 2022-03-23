67ede248be6d328559823f1e82363359e13b8b1e
Juan Pablo <jpmontoya19@gmail.com>
2022-03-23 13:24:53 -0500

    models_users

---
+++ fastAPI/twitter-api-fastapi/main.py
@@ -0,0 +1,49 @@
+# Python
+from typing import Optional
+from uuid import UUID
+from datetime import date
+
+# Pydantic
+from pydantic import BaseModel
+from pydantic import EmailStr
+from pydantic import Field
+
+# FastAPI
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# Models
+class UserBase(BaseModel):
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(...)
+
+class UserLogin(UserBase):
+    password: str = Field(
+        ...,
+        min_length=8,
+    )
+
+class User(UserBase):
+    firs_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    birth_date: Optional[date] = Field(default=None)
+
+class Tweet(BaseModel):
+    pass
+
+@app.get(path="/")
+def home():
+    return {"Twitter API": "Working"}
+
+if __name__ == "__main__":
+   import uvicorn
+   uvicorn.run('main:app', host="127.0.0.1", port=8000, reload=True)
