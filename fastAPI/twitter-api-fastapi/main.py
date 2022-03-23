f5c5689f0bb08759cb730bbe7c5c014ee60ae492
Juan Pablo <jpmontoya19@gmail.com>
2022-03-23 14:28:51 -0500

    users_signup

--- fastAPI/twitter-api-fastapi/main.py
+++ fastAPI/twitter-api-fastapi/main.py
@@ -1,4 +1,5 @@
 # Python
+import json
 from typing import Optional, List
 from uuid import UUID
 from datetime import date, datetime
@@ -11,6 +12,7 @@ from pydantic import Field
 # FastAPI
 from fastapi import FastAPI
 from fastapi import status
+from fastapi import Body
 
 app = FastAPI()
 
@@ -66,7 +68,7 @@ class Tweet(BaseModel):
     summary="Register a User",
     tags=["Users"]
 )
-def signup():
+def signup(user: UserRegister = Body(...)):
     """
     Signup
     
@@ -81,9 +83,17 @@ def signup():
         - email: EmailStr
         - firs_name: str
         - last_name: str
-        _ birth_date: str
+        _ birth_date: date
     """
-    
+    with open("users.json", "r+", encoding="utf-8") as f:
+        results = json.load(f)
+        user_dict = user.dict()
+        user_dict["user_id"] = str(user_dict["user_id"])
+        user_dict["birth_date"] = str(user_dict["birth_date"])
+        results.append(user_dict)
+        f.seek(0)
+        json.dump(results, f)
+        return user
 
 ### Login a user
 @app.post(
