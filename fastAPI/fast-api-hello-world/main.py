3c344bd96daa6ce17d05f70d0eaf4441955a6be0
Juan Pablo <jpmontoya19@gmail.com>
2022-03-23 11:55:23 -0500

    files

--- fastAPI/fast-api-hello-world/main.py
+++ fastAPI/fast-api-hello-world/main.py
@@ -8,8 +8,8 @@ from pydantic import Field
 
 # FastAPI
 from fastapi import FastAPI
-from fastapi import status
-from fastapi import Body, Query, Path, Form, Header, Cookie
+from fastapi import status, UploadFile
+from fastapi import Body, Query, Path, Form, Header, Cookie, File
 
 app = FastAPI()
 
@@ -197,3 +197,16 @@ def contact(
     ads: Optional[str] = Cookie(default=None)
 ):
     return user_agent
+
+# Files
+@app.post(
+    path="/post_image"
+)
+def post_image(
+    image: UploadFile = File(...)
+):
+    return {
+        "Filename": image.filename,
+        "Format": image.content_type,
+        "Size(kb)": round(len(image.file.read())/1024, ndigits=2)
+    }
