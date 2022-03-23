288afc392d32eab9de545b5063b924ec35e1fc44
Juan Pablo <jpmontoya19@gmail.com>
2022-03-23 15:09:37 -0500

    post_tweets

--- fastAPI/twitter-api-fastapi/main.py
+++ fastAPI/twitter-api-fastapi/main.py
@@ -1,7 +1,6 @@
 # Python
 import json
 from typing import Optional, List
-from unittest import result
 from uuid import UUID
 from datetime import date, datetime
 
@@ -188,8 +187,37 @@ def home():
     summary="Post a tweet",
     tags=["Tweets"]
 )
-def post():
-    pass
+def post(tweet: Tweet = Body(...)):
+    """
+    Post a tweet
+    
+    This path operations post a tweet in the app
+
+    Parameters:
+        - Request body parameter
+            - tweet: Tweet
+    
+    Returns a json with the basic tweet information:
+        - tweet_id: UUID
+        - content: str
+        - created_at: datetime
+        - updated_at: Optional[datetime]
+        - by: User
+    """
+    with open("tweets.json", "r+", encoding="utf-8") as f:
+        result = json.load(f)
+        tweet_dict = tweet.dict()
+        tweet_dict["tweet_id"] = str(tweet_dict["tweet_id"])
+        tweet_dict["created_at"] = str(tweet_dict["created_at"])
+        tweet_dict["updated_at"] = str(tweet_dict["updated_at"])
+
+        tweet_dict["by"]["user_id"] = str(tweet_dict["by"]["user_id"])
+        tweet_dict["by"]["birth_date"] = str(tweet_dict["by"]["birth_date"])
+
+        result.append(tweet_dict)
+        f.seek(0)
+        json.dump(result, f)
+        return tweet
 
 ### Show a tweet
 @app.get(
