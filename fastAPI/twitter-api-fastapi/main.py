b6a2c9a36a6ba100eeac40ed2fd87710aa194fc0
Juan Pablo <jpmontoya19@gmail.com>
2022-03-23 14:36:10 -0500

    show_users

--- fastAPI/twitter-api-fastapi/main.py
+++ fastAPI/twitter-api-fastapi/main.py
@@ -1,6 +1,7 @@
 # Python
 import json
 from typing import Optional, List
+from unittest import result
 from uuid import UUID
 from datetime import date, datetime
 
@@ -115,7 +116,23 @@ def login():
     tags=["Users"]
 )
 def show_all_users():
-    pass
+    """
+    This path operation shows all users in the app
+
+    Parameters:
+        -
+    
+    Returns a json list with all users in the app, whit the following keys
+        - user_id: UUID
+        - email: EmailStr
+        - firs_name: str
+        - last_name: str
+        _ birth_date: date
+    """
+
+    with open("users.json", "r", encoding="utf-8") as f:
+        result = json.load(f)
+        return result
 
 ### Show a user
 @app.get(
